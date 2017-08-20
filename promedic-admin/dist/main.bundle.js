63bec7ba7b2a9dd79e73ca7b37bed55e0f2fed8e
Oladapo LAWORE <dapolawore@gmail.com>
2017-08-20 18:22:49 +0100

    updated chart tooltip & delete functionality for responder,etc

--- promedic-admin/dist/main.bundle.js
+++ promedic-admin/dist/main.bundle.js
@@ -867,7 +867,7 @@ var ClientDiseaseChartComponent = (function () {
                 enabled: false
             },
             tooltip: {
-                pointFormat: 'Population in 2008: <b>{point.y} millions</b>'
+                pointFormat: _data[0][1] + ' clients have  ' + _data[0][0] //'Population in 2008: <b>{point.y} millions</b>'
             },
             series: [{
                     name: 'Population',
